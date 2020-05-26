b15339c0dfa9ba07973b9c0e963a870206962a31
Chester Waye <cwaye2000@gmail.com>
2020-05-26 11:37:52 -0400

    Prj Complete
    Submit Proj

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -17,11 +17,12 @@
  * Define Global Variables
  * 
 */
-const navMenu = document.getElementById('navbar_list');
+const navMenu = document.getElementById('navbar__list');
+
 
 const sec = document.querySelectorAll('section');
 
-
+const secs = document.getElementsByTagName("section");
 /**
  * End Global Variables
  * Start Helper Functions
@@ -37,27 +38,32 @@ const sec = document.querySelectorAll('section');
 */
 
 // build the nav
-const navBar = () => {
-    let navs = '';
-    
-    sec.forEach(section => {
-        const sectionId = section.id;
-        const secData = section.dataset.nav;
 
-        navs += '<li><a class="menu__link" href="#${sectionId}">${secData}</a></li>';
-    });
-    
-    navMenu.innerHTML = navs;
-};
-
-navBar();
-
+ function buildNav() {
+    for (let sectionItem of secs) {
+        let li = document.createElement("li");
+        li.className = "menu__link";
+        li.innerHTML = `<a href="#${sectionItem.id}">${sectionItem.dataset.nav}</a>`;
+        navMenu.appendChild(li);
+    }
+}
+buildNav();
+ 
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
 /**
  * End Main Functions
@@ -73,62 +79,11 @@ navBar();
 
 // Add class 'active' to section when it is near top of viewport
 
+
+
+
+
 // Make sections active
 
+
 // Build a global variable
-
-/* const navbarMenu = document.getElementById('navbar_list');
-
-function narbarMenu() {
-
-    var sections = document.getElementsByTagName('SECTION');
-
-    let i;
-
-    for (i = 0; i < sections.length; i++) {
-
-        nodeName = sections[i].getAttribute('data-nav');
-
-        link = sections[i].textContent;
-
-    }
-
-    var node = document.createElement('LI');
-
-    var a = document.createElement('A');
-
-    a.appendChild(nodeName);
-
-    a.appendChild(link);
-
-    node.appendChild(a);
-
-    node.setAttribute('style', '.navbar_menu');
-
-    navbarMenu.appendChild(node);
-
-}
-
-document.body.appendChild(navbarMenu);
-
-navbarMenu.setAttribute('style', '.menu_link');
-
-}
-
-navbarMeny();
-document.addEventListener("scroll", function () {
-    makeActive();
-});
-
-function makeActive() {
-    for (const section of sections) {
-        const box = section.getBoundingClientRect();
-        // You can play with the values in the "if" condition to further make it more accurate. 
-        if (box.top <= 150 && box.bottom >= 150) {
-            // Apply active state on the current section and the corresponding Nav link.
-        } else {
-            // Remove active state from other section and corresponding Nav link.
-        }
-    }
-}
- */
